18b7411670d290312811b37f7c33f64baaa3b80f
MetyolckinAA <metyolckin.an@yandex.ru>
2025-07-05 20:51:39 +0300

    Работа с assert

---
+++ test/test_pytest.py
@@ -0,0 +1,15 @@
+
+class TestUserLogin:
+    def test1(self):
+        ...
+
+    def test2(self):
+        ...
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+    assert (3 + 3) == 6
+    assert (4 + 4) == 8
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "2 + 2 != 5"
