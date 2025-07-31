1fa9cadd292ed8230e6daf8493e8a568c5fc08da
MetyolckinAA <metyolckin.an@yandex.ru>
2025-07-31 17:38:24 +0300

    Remove saving videosand tracing from allure report

--- config.py
+++ config.py
@@ -37,8 +37,8 @@ class Settings(BaseSettings):
     browsers: list[Browser]
     test_user: TestUser
     test_data: TestData
-    videos_dir: DirectoryPath
-    tracing_dir: DirectoryPath
+    # videos_dir: DirectoryPath
+    # tracing_dir: DirectoryPath
     allure_results_dir: DirectoryPath
     browser_state_file: FilePath
 
@@ -47,19 +47,19 @@ class Settings(BaseSettings):
 
     @classmethod
     def initialize(cls) -> Self:
-        videos_dir = DirectoryPath("./videos")
-        tracing_dir = DirectoryPath("./tracing")
+        # videos_dir = DirectoryPath("./videos")
+        # tracing_dir = DirectoryPath("./tracing")
         allure_results_dir = DirectoryPath("./allure-results")
         browser_state_file = FilePath("browser-state.json")
 
-        videos_dir.mkdir(exist_ok=True)
-        tracing_dir.mkdir(exist_ok=True)
+        # videos_dir.mkdir(exist_ok=True)
+        # tracing_dir.mkdir(exist_ok=True)
         allure_results_dir.mkdir(exist_ok=True)
         browser_state_file.touch(exist_ok=True)
 
         return Settings(
-            videos_dir=videos_dir,
-            tracing_dir=tracing_dir,
+            # videos_dir=videos_dir,
+            # tracing_dir=tracing_dir,
             allure_results_dir=allure_results_dir,
             browser_state_file=browser_state_file
         )
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -14,16 +14,16 @@ def initialize_playwright_page(
     context = browser.new_context(
         base_url=settings.get_base_url(),
         storage_state=storage_state,
-        record_video_dir=settings.videos_dir
+        # record_video_dir=settings.videos_dir
     )
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    # context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
     mock_static_resources(page)
 
     yield page
 
-    context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
+    # context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
     browser.close()
 
-    allure.attach.file(settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+    # allure.attach.file(settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
+    # allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
